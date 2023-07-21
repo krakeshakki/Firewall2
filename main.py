e3f1c9adf4d0381bd929b1596270602c8d7d7379
krakeshakki <140057478+krakeshakki@users.noreply.github.com>
2023-07-21 10:32:12 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+import os
+
+import unittest
+
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__)+ '/src' })})
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__) + '/tests' )))
+
+#from core import get_answer
+
+from core import sain
+
+if name --__main__':
+
+f = open('packets/tcp.txt","r")
+
+E = open('packets/udp.txt','r')
+
+main(f)
